1a06715ea0a81016dd720f33bd438ac40c6be624
Stephen <s.cormican7@icloud.com>
2025-07-23 11:51:30 +0100

    Adding one new file

--- 3. Bill.py
+++ 3. Bill.py
@@ -1,3 +1,12 @@
+#Lines 10 - 12 have the same idea behind them regarding variables as in 
+#'1. Variables' and '2. Scores' except that the mathematical calculation is different.
+#Line 8 uses the 'print' function for output, which includes the variable in line 7.
+#However, within the parentheses, it adds a string prior to the variable name.
+#This can be achieved by opening double quotations ("") inside of the function
+#parentheses, where within the double quotations, a string can be entered relevant
+#to what will be output. Then, after the closing quotation, the string and variable
+#should be separated by a comma (,) as seen in line 13.
+
 quantity = 9
 price = 3
 bill = quantity * price
