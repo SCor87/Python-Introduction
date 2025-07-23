52a56177a0c9eece84dc7aa37ea47b7fca3bc51f
Stephen <s.cormican7@icloud.com>
2025-07-23 10:45:42 +0100

    Adding comments to file for clarification of coded lines.

--- 1. Variables.py
+++ 1. Variables.py
@@ -1,3 +1,12 @@
+#Variables are assigned by using the assignment operator (=)
+#This stores the value on the right hand side into the variable name on the left hand side.
+#eg line 10 - A is the variable, 20 is the value.
+#Variables are case sensitive. If we later tried to call 'a' in the same example, we would get an error.
+#Variables can be reassigned. Instead of 'A=20', you can reassign to 'A=4'
+#Line 12 is assigning the value of 'A' and 'B' to a new variable called 'C' by carrying
+#out a simple mathematical calculation. The mathematical expression '+' is used for this calculation.
+#Variables are output by calling on the 'print' function, as seen in line 13.
+
 A=20
 B=89
 C=A+B
